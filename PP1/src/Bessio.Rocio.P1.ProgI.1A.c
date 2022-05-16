2f70a86aa224388fafcbea0ebc5bdc72bc6c2617
rociobessio <rocibessio@gmail.com>
2022-05-16 09:08:38 -0300

    creando repo

---
+++ PP1/src/Bessio.Rocio.P1.ProgI.1A.c
@@ -0,0 +1,20 @@
+/*
+ ============================================================================
+ Name        : 1A.c
+ Author      : Rocio Bessio
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	setbuf(stdout,NULL);
+
+
+	return EXIT_SUCCESS;
+}
